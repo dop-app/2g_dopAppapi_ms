cee51b3d13373310fe92bb4cc41f8c91747c3c32
wolfstain <dahoyosr@unal.edu.co>
2018-05-02 23:46:45 +0000

    s

--- src/gustos/server.js
+++ src/gustos/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.GUSTOS_URL || '35.227.46.47';
+export const url = process.env.GUSTOS_URL || 'gustos-ms';
 export const port = process.env.GUSTOS_PORT || '4006';
 export const entryPoint = process.env.GUSTOS_ENTRY || '';
--- src/usuarios/server.js
+++ src/usuarios/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.USUARIOS_URL || 'users-ms_1';
+export const url = process.env.USUARIOS_URL || 'users-ms';
 export const port = process.env.USUARIOS_PORT || '4001';
 export const entryPoint = process.env.USUARIOS_ENTRY || 'users';
