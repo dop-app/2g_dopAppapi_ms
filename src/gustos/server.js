5839f3f007f2caf7ba95afa1e91e14082fc535a0
wolfstain <dahoyosr@unal.edu.co>
2018-05-21 03:55:14 +0000

    cambios nombres ms

--- src/gustos/server.js
+++ src/gustos/server.js
@@ -1,4 +1,4 @@
-export const url = process.env.GUSTOS_URL || 'gustos_ms';
+export const url = process.env.GUSTOS_URL || 'gustos-ms';
 //export const url = process.env.GUSTOS_URL || '35.227.46.47';
 export const port = process.env.GUSTOS_PORT || '4006';
 export const entryPoint = process.env.GUSTOS_ENTRY || '';
